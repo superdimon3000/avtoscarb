b1a35e1b7d8fe0b2d2b17eec1973d4ee688ee017
superdimon3000 <dimon43168@gmail.com>
2023-05-09 17:44:00 +0300

    Изменена регистрация

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
+    path('profile/<str:username>/', views.user_profile, name='user_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,42 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.models import User
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'shop/list.html', {'form': form})
 
 def register(request):
-    if request.method != 'POST':
-        form = UserCreationForm()
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            
+            new_user = user_form.save(commit=False)
+            
+            new_user.set_password(user_form.cleaned_data['password'])
+            
+            new_user.save()
+            return render(request, 'registration\\register_done.html', {'new_user': new_user})
     else:
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            login(request, new_user)
-            return redirect('shop:index')
-    context = {'form': form}
-    return render(request, 'registration/register.html', context)
+        user_form = UserRegistrationForm()
+    return render(request, 'registration\\register.html', {'user_form': user_form})
+
+def user_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'profile.html', {'user': user})
